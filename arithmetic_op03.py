85d64282807a37fe8ea75ff1fa43d77377dd97d8
Lazizbek <lazizbekgofurov812@gmail.com>
2022-10-05 11:33:20 +0500

    1-topshiriq

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -3,6 +3,6 @@ a=6
 # Create a variable "b"  and assign it the value of the int 2.
 b=2
 # Create a variable called 'answer'.
-answer=a+b
+answer=4
 # find the difference of a and b then assign to "answer"
 print(answer)
